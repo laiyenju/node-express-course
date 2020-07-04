c48595fda231041515af9712fdfabe65e96c8f9e
Lai Yen Ju <yenju.lai@icloud.com>
2020-07-04 10:19:15 +0800

    add dynamic GET routes

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const mockUserData=[
   {name:'Jill'}
   ]
 
-app.get('/users', function(req,res){
+app.get('/users', function(req,res){  // 瀏覽器進入 /users，取得的是 mockUserData 的所有資料
   res.json({
     success: true,
     message: 'successfully got users. Nice!',
     users: mockUserData
   })
+})
+
+app.get('/users/:id',function(req,res){  // 瀏覽器進入 /users/<id>，取得的是 mockUserData 個別 id 資料
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 app.listen(8000, function(){
