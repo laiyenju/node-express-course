69c7a78efa9a8cbda87e07d78cdb43525aff6b9c
Lai Yen Ju <yenju.lai@icloud.com>
2020-07-04 10:27:13 +0800

    add login POST routes

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');  // import the Express library
 const app = express();
+const bodyParser = require('body-parser');  // import body-parser for POST
+
+app.use(bodyParser.json());
 
 const mockUserData=[
   {name:'Mark'},
   {name:'Jill'}
-  ]
+]
 
-app.get('/users', function(req,res){  // 瀏覽器進入 /users，取得的是 mockUserData 的所有資料
+
+// 瀏覽器進入 /users，取得的是 mockUserData 的所有資料
+app.get('/users', function(req,res){
   res.json({
     success: true,
     message: 'successfully got users. Nice!',
@@ -14,13 +19,39 @@ app.get('/users', function(req,res){  // 瀏覽器進入 /users，取得的是 m
   })
 })
 
-app.get('/users/:id',function(req,res){  // 瀏覽器進入 /users/<id>，取得的是 mockUserData 個別 id 資料
+
+// 瀏覽器進入 /users/<id>，回傳與網址後綴對應的內容
+app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
 		success: true,
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+
+// POST：使用者登入
+app.post('/login',function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+
+  const mockUsername="billyTheKid";
+  const mockPassword="superSecret";
+
+  // 驗證登入資料
+  if (username===mockUsername && password===mockPassword){
+       res.json({
+          success: true,
+          message: 'password and username match!',
+          token: 'encrypted token goes here'
+       })
+  } else {
+       res.json({
+          success: false,
+          message: 'password and username do not match'
+       })
+  }
 })
 
 app.listen(8000, function(){
